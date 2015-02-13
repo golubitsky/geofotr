80836919a1046b87c9376a8658dc18399dcc160b
Mike Golubitsky <mike617@gmail.com>
2015-02-13 14:24:47 -0800

    update scroll to photo and begin work on photo show

--- app/assets/javascripts/utils/utils.js
+++ app/assets/javascripts/utils/utils.js
@@ -2,11 +2,13 @@ Geofotr.isEnterKeypress = function (event) {
   event.preventDefault();
   return event.keycode === 13;
 }
+//removed this from line 11
+//+ $element.outerHeight(true)
 
 Geofotr.scrollTo = function($element){
   debugger
   $("#content").animate({
-      scrollTop: $element.position().top + $element.outerHeight(true)
+      scrollTop: $element.position().top
     }, 350);
 }
 
